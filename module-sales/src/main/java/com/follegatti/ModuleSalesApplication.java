d19dbfa0e002b7ac10f5549b5a60057c254e4096
Carlos Enrique Follegatti Velarde <cfollega@emeal.nttdata.com>
2023-12-28 01:29:10 -0500

    configuracion inicial del proyecto

---
+++ module-sales/src/main/java/com/follegatti/ModuleSalesApplication.java
@@ -0,0 +1,13 @@
+package com.follegatti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleSalesApplication.class, args);
+	}
+
+}
---
+++ module-sales/src/test/java/com/follegatti/ModuleSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.follegatti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
